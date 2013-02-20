4ea6f7ced4938335f7cf2267ed4842517761da7f
Kristofor Maynard <k.maynard@unh.edu>
2013-02-20 00:31:32 -0500

    refactor mayavi code

--- viscid/plot/mvi.py
+++ viscid/plot/mvi.py
@@ -8,43 +8,40 @@ from tvtk.api import tvtk
 
 from .. import field
 
-'''
-def data_source(fld):
-    """ get a data source from a scalar field """
-    dat = tvtk.RectilinearGrid()
-
-    suffix = "cc" if fld.center == "Cell" else ""
-    dat.x_coordinates = fld.crds['x' + suffix]
-    dat.y_coordinates = fld.crds['y' + suffix]
-    dat.z_coordinates = fld.crds['z' + suffix]
-
-    if isinstance(fld, field.ScalarField):
-        dat.point_data.scalars = np.ravel(fld.dat)
-        dat.point_data.scalars.name = fld.name
-    elif isinstance(fld, field.VectorField):
-        pass
-
-    return VTKDataSource(data=dat)
-
-    rr = tvtk.RectilinearGrid()
-
-    rr.point_data.scalars.name = 'rr'
-    rr.dimensions = collection[0]['rr'].shape[::-1]
-    #print(collection[0]['coords'][:])
-    rr_src = VTKDataSource(data=rr)
-
-    b = tvtk.RectilinearGrid()
-    bdata = np.empty((np.prod(collection[0]['bx'].shape), 3))
-    for i in range(3):
-        bdata[:,i] = np.asarray(collection[0]['b'][:,:,:,i], dtype='f').ravel()
-    b.point_data.vectors = bdata
-    b.dimensions = collection[0]['b'].shape[:-1][::-1]
-
-    b_src = VTKDataSource(data=b)
-    collection.unload()
-
-    pass
-'''
+# def data_source(fld):
+#     """ get a data source from a scalar field """
+#     dat = tvtk.RectilinearGrid()
+#
+#     suffix = "cc" if fld.center == "Cell" else ""
+#     dat.x_coordinates = fld.crds['x' + suffix]
+#     dat.y_coordinates = fld.crds['y' + suffix]
+#     dat.z_coordinates = fld.crds['z' + suffix]
+#
+#     if isinstance(fld, field.ScalarField):
+#         dat.point_data.scalars = np.ravel(fld.dat)
+#         dat.point_data.scalars.name = fld.name
+#     elif isinstance(fld, field.VectorField):
+#         pass
+#
+#     return VTKDataSource(data=dat)
+#
+#     rr = tvtk.RectilinearGrid()
+#
+#     rr.point_data.scalars.name = 'rr'
+#     rr.dimensions = collection[0]['rr'].shape[::-1]
+#     #print(collection[0]['coords'][:])
+#     rr_src = VTKDataSource(data=rr)
+#
+#     b = tvtk.RectilinearGrid()
+#     bdata = np.empty((np.prod(collection[0]['bx'].shape), 3))
+#     for i in range(3):
+#         bdata[:,i] = np.asarray(collection[0]['b'][:,:,:,i],
+#                                 dtype='f').ravel()
+#     b.point_data.vectors = bdata
+#     b.dimensions = collection[0]['b'].shape[:-1][::-1]
+#
+#     b_src = VTKDataSource(data=b)
+#     collection.unload()
 
 def field_to_source(fld):
     grid = tvtk.RectilinearGrid()
@@ -86,17 +83,18 @@ def field_to_source(fld):
 
     return src
 
-def mlab_earth(pipeline, daycol=(1,1,1), nightcol=(0,0,0), res=15):
+def mlab_earth(pipeline, daycol=(1, 1, 1), nightcol=(0, 0, 0), res=15):
     from mayavi.sources.builtin_surface import BuiltinSurface
 
     night = BuiltinSurface(source='sphere', name='night')
-    night.data_source.set(center=(0,0,0), radius=1.0, start_theta=270, end_theta=90,
-            theta_resolution=res, phi_resolution=res)
+    night.data_source.set(center=(0, 0, 0), radius=1.0, start_theta=270,
+                          end_theta=90, theta_resolution=res,
+                          phi_resolution=res)
     pipeline.surface(night, color=nightcol)
 
     day = BuiltinSurface(source='sphere', name='day')
-    day.data_source.set(center=(0,0,0), radius=1.0, start_theta=90, end_theta=270,
-            theta_resolution=res, phi_resolution=res)
+    day.data_source.set(center=(0, 0, 0), radius=1.0, start_theta=90,
+                        end_theta=270, theta_resolution=res, phi_resolution=res)
     pipeline.surface(day, color=daycol)
 
 ##
