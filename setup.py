fc21ae2fcc269c189098566da77a2d1694f847c3
Kristofor Maynard <k.maynard@unh.edu>
2013-02-24 12:24:52 -0500

    bump version to 0.32

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ for d in cy_defs:
                            extra_link_args=cy_ldflags)]
 
 setup(name='viscid',
-      version='0.31',
+      version='0.32',
       description='Visualization in python',
       author='Kris Maynard',
       author_email='k.maynard@unh.edu',
