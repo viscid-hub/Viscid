d64eeeecad76264b646dd7eecf53f3fdbe675430
Kristofor Maynard <k.maynard@unh.edu>
2013-09-27 00:56:28 -0400

    kludgy support for plotting derived quantities

--- viscid/vlab.py
+++ viscid/vlab.py
@@ -24,7 +24,7 @@ def _do_multiplot(tind, grid, plot_vars, global_popts=None, share_axes=False,
     out_prefix = kwopts.get("out_prefix", None)
     out_format = kwopts.get("out_format", "png")
     selection = kwopts.get("selection", None)
- 
+
     nrows = len(plot_vars)
     ncols = 1
     if transpose:
@@ -52,30 +52,44 @@ def _do_multiplot(tind, grid, plot_vars, global_popts=None, share_axes=False,
                 fld_slc = ",".join([fld_slc, selection])
             else:
                 fld_slc = selection
-        with grid[fld_name] as fld:
-            # print("fld_time:", fld.time)
-            row = i
-            col = 0
-            if transpose:
-                row, col = col, row
-            ax = plt.subplot2grid((nrows, ncols), (row, col),
-                                  sharex=shareax, sharey=shareax)
-            if i == 0 and share_axes:
-                shareax = ax
-            
-            if not "plot_opts" in fld_meta[1]:
-                fld_meta[1]["plot_opts"] = global_popts
-            elif global_popts is not None:
-                fld_meta[1]["plot_opts"] = "{0},{1}".format(
-                                    fld_meta[1]["plot_opts"], global_popts)
-            mpl.plot(fld, selection=fld_slc, **fld_meta[1])
+
+        # print("fld_time:", fld.time)
+        row = i
+        col = 0
+        if transpose:
+            row, col = col, row
+        ax = plt.subplot2grid((nrows, ncols), (row, col),
+                              sharex=shareax, sharey=shareax)
+        if i == 0 and share_axes:
+            shareax = ax
+
+        if not "plot_opts" in fld_meta[1]:
+            fld_meta[1]["plot_opts"] = global_popts
+        elif global_popts is not None:
+            fld_meta[1]["plot_opts"] = "{0},{1}".format(
+                fld_meta[1]["plot_opts"], global_popts)
+
+        # FIXME: this method of plotting T, beta, etc. really sucks
+        # also... do i really need with statements here?
+        if fld_name == "T":
+            with grid["pp"] as pp, grid["rr"] as rr:
+                mpl.plot(pp / rr, selection=fld_slc, **fld_meta[1])
+        elif fld_name == "beta":
+            with grid["bx"] as bx, grid["by"] as by, \
+                 grid["bz"] as bz, grid["pp"] as pp:
+                beta = pp / (bx**2 + by**2 + bz**2)
+                mpl.plot(beta, selection=fld_slc, **fld_meta[1])
+        else:
+            with grid[fld_name] as fld:
+                mpl.plot(fld, selection=fld_slc, **fld_meta[1])
         # print("fld cache", grid[fld_meta[0]]._cache)
+
     hrs = int(grid.time / 3600)
     mins = int((grid.time / 60) % 60)
     secs = grid.time % 60
     # plt.suptitle("t = {0:.2f}".format(grid.time))
     plt.suptitle("\nt = {0}:{1:02}:{2:05.2f}".format(hrs, mins, secs))
-    # mpl.tighten()
+    mpl.tighten()
 
     if out_prefix:
         plt.savefig("{0}_{1:06d}.{2}".format(out_prefix, tind + 1, out_format))
