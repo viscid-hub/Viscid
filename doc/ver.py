bed1743cf9b25170468a07e70af417293704517f
Kristofor Maynard <k.maynard@unh.edu>
2013-10-08 23:23:42 -0400

    bump version number to 0.50.0

--- doc/ver.py
+++ doc/ver.py
@@ -1,7 +1,7 @@
 from distutils.version import LooseVersion
 
 # The short X.Y version.
-version = "0.46.0"
+version = "0.50.0"
 
 # The full version, including alpha/beta/rc tags.
 release = LooseVersion(version)
