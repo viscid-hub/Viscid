0cfd0ff83f87f3246bc8d7a992faec94b7ac5536
Kristofor Maynard <k.maynard@unh.edu>
2014-03-19 17:11:42 -0400

    enable easier overriding of topology color mapping

--- viscid/calculator/topology.py
+++ viscid/calculator/topology.py
@@ -11,20 +11,18 @@ TOPOLOGY_SW = 8  # no translation needed
 TOPOLOGY_INVALID = [3, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15]
 TOPOLOGY_OTHER = range(32, 512)  # >= 16
 
+color_map = {TOPOLOGY_CLOSED: (0.0, 0.8, 0.0),
+             TOPOLOGY_OPEN_NORTH: (0.0, 0.0, 0.7),
+             TOPOLOGY_OPEN_SOUTH: (0.7, 0.0, 0.0),
+             TOPOLOGY_SW: (0.7, 0.7, 0.7)
+            }
+
 def color_from_topology(topology):
     """ topology is an integer described by calculator.streamline.TOPOLOGY_*
     returns a color tuple
     Note: to override this color scheme, just set
           topoloy.color_from_topology = some_function """
-    if topology == TOPOLOGY_CLOSED:
-        return (0.0, 0.8, 0.0)
-    elif topology == TOPOLOGY_OPEN_NORTH:
-        return (0.0, 0.0, 0.7)
-    elif topology == TOPOLOGY_OPEN_SOUTH:
-        return (0.7, 0.0, 0.0)
-    elif topology == TOPOLOGY_SW:
-        return (0.7, 0.7, 0.7)
-    # elif topology == TOPOLOGY_CYCLIC:
-    #     return (1.0, 1.0, 1.0)
-    else:
+    try:
+        return color_map[topology]
+    except KeyError:
         return (0.0, 0.0, 0.0)
