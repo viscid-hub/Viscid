c8e56fd2c5f91f4f5b31220d9eb1a98cf881d989
Kristofor Maynard <k.maynard@unh.edu>
2014-03-27 15:04:37 -0400

    add follow_fluid to vlab

--- viscid/vlab.py
+++ viscid/vlab.py
@@ -15,6 +15,15 @@ except ImportError:
 from . import parallel
 from . import field
 from . import coordinate
+from .calculator import seed
+
+# these compiled are needed for fluid following
+try:
+    from .calculator import cycalc
+    from .calculator import streamline
+    from .calculator.streamline import streamlines
+except ImportError:
+    pass
 
 def get_dipole(m=None, l=None, h=None, n=None, twod=False):
     dtype = 'float64'
@@ -183,6 +192,111 @@ def _do_multiplot(tind, grid, plot_vars, global_popts=None, share_axes=False,
         plt.show()
     plt.clf()
 
+def follow_fluid(vfile, time_slice, initial_seeds, plot_function,
+                 stream_opts, add_seed_cadence=0.0, add_seed_pts=None,
+                 speed_scale=1.0):
+    """ vfile: a vFile object that we can call iter_times on
+    time_slice: string, slice notation, like 1200:2400:1
+    initial_seeds: any SeedGen object
+    plot_function: function that is called each time step, arguments
+                   should be exactly: (i [int], grid, v [Vector Field],
+                                       v_lines [result of streamline trace],
+                                       root_seeds [SeedGen])
+    stream_opts: a dict of streamline options for flow lines
+    add_seed_cadence: how often to add the add_seeds points
+    add_seeds: an n x 3 ndarray of n points to add every add_seed_cadence (zyx)
+    speed_scale: speed_scale * v should be in units of ds0 / dt
+    stream_opts must have ds0 and max_length, maxit will be automatically calculated
+    """
+    times = np.array([grid.time for grid in vfile.iter_times(time_slice)])
+    dt = np.roll(times, -1) - times  # Note: last element makes no sense
+    root_seeds = initial_seeds
+
+    # setup maximum number of iterations for a streamline
+    if "ds0" in stream_opts and "max_length" in stream_opts:
+        max_length = stream_opts["max_length"]
+        ds0 = stream_opts["ds0"]
+        stream_opts["maxit"] = (max_length // ds0) + 1
+    else:
+        raise KeyError("ds0 and max_length must be keys of stream_opts, "
+                       "otherwise I don't know how to follow the fluid")
+
+    # iterate through the time steps from time_slice
+    for i, grid in enumerate(vfile.iter_times(time_slice)):
+        if i == 0:
+            last_add_time = grid.time
+
+        root_pts = _follow_fluid_step(i, dt[i], grid, root_seeds,
+                                      plot_function, stream_opts, speed_scale)
+
+        # maybe add some new seed points to account for those that have left
+        if add_seed_pts and abs(grid.time - last_add_time) >= add_seed_cadence:
+            root_pts = np.concatenate([root_pts, add_seed_pts.T], axis=1)
+            last_add_time = grid.time
+        root_seeds = seed.Point(root_pts)
+
+    return None
+
+def _follow_fluid_step(i, dt, grid, root_seeds, plot_function, stream_opts,
+                       speed_scale):
+    logging.info("working on timestep {0} {1}".format(i, grid.time))
+    v = grid["v"]
+    logging.debug("finished reading V field")
+
+    logging.debug("calculating new streamline positions")
+    flow_lines = streamlines(v, root_seeds,  # pylint: disable=W0142
+                             output=streamline.OUTPUT_STREAMLINES,
+                             **stream_opts)[0]
+
+    logging.debug("done with that, now i'm plotting...")
+    plot_function(i, grid, v, flow_lines, root_seeds)
+
+    ############################################################
+    # now recalculate the seed positions for the next timestep
+    logging.debug("finding new seed positions...")
+    root_pts = root_seeds.genr_points()
+    valid_pt_inds = []
+    for i in range(root_pts.shape[1]):
+        valid_pt = True
+
+        # get the index of the root point in teh 2d flow line array
+        dist = flow_lines[i] - root_pts[:, [i]]
+        root_ind = np.argmin(np.sum(dist**2, axis=0))
+        # print("!!!", root_pts[:, i], "==", flow_lines[i][:, root_ind])
+        # interpolate velocity onto teh flow line, and get speed too
+        v_interp = cycalc.interp_trilin(v, seed.Point(flow_lines[i]))
+        speed = np.sqrt(np.sum(v_interp * v_interp, axis=1))
+        direction = int(dt / np.abs(dt))
+
+        # this is a super slopy way to integrate velocity
+        # keep marching along the flow line until we get to the next timestep
+        t = 0.0
+        ind = root_ind
+        while np.abs(t) < np.abs(dt):
+            ind += direction
+            if ind < 0 or ind >= v_interp.shape[0]:
+                # set valid_pt to True if you want to keep that point for
+                # future time steps, but most likely if we're here, the seed
+                # has gone out of our region of interest
+                ind = max(min(ind, v_interp.shape[0] - 1), 0)
+                valid_pt = False
+                logging.warning("OOPS: ran out of streamline, increase "
+                                "max_length when tracing flow lines if this "
+                                "is unexpected")
+                break
+
+            t += stream_opts["ds0"] / (speed_scale * speed[ind])
+
+        root_pts[:, i] = flow_lines[i][:, ind]
+        if valid_pt:
+            valid_pt_inds.append(i)
+
+    # remove seeds that have flown out of our region of interest
+    # (aka, beyond the flow line we drew)
+    root_pts = root_pts[:, valid_pt_inds]
+
+    logging.debug("ok, done with all that :)")
+    return root_pts
 
 ##
 ## EOF
