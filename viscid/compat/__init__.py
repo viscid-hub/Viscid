19e874a2ab7f474f12a1e6cefc7fac6066500b23
Kristofor Maynard <k.maynard@unh.edu>
2015-01-15 13:04:02 -0500

    add compatibility import for zip_longest

--- viscid/compat/__init__.py
+++ viscid/compat/__init__.py
@@ -8,8 +8,10 @@ except ImportError:
 
 try:
     from itertools import izip  # pylint: disable=no-name-in-module
+    from itertools import izip_longest  # pylint: disable=no-name-in-module
 except ImportError:
     izip = zip
+    from itertools import zip_longest as izip_longest
 
 # Taken from the six module
 # Copyright (c) 2010-2014 Benjamin Peterson
