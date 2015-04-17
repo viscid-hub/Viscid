aa5a5b10e710c216729a451307853aace3961e7a
Kristofor Maynard <k.maynard@unh.edu>
2015-04-17 00:03:24 -0400

    pretty iof example/test

--- test/test_iof.py
+++ test/test_iof.py
@@ -27,14 +27,27 @@ def main():
 
     fac_tot = 1e9 * iono_file["fac_tot"]
 
+    plot_args = dict(projection="polar",
+                     lin=[-300, 300],
+                     bounding_lat=35.0,
+                     drawcoastlines=True,  # for basemap only, probably will never be used
+                     title="Total FAC\n",  # make a title, or if a string, use the string as title
+                     gridec='gray',
+                     label_lat=True,
+                     label_mlt=True,
+                     colorbar=dict(pad=0.1)  # pad the colorbar away from the plot
+                    )
+
     ax1 = plt.subplot(121, projection='polar')
-    mpl.plot(fac_tot, ax=ax1, hemisphere="north", style="pcolormesh",
-             plot_opts="lin_-300_300", g=0, #extend="both",
-             levels=50, drawcoastlines=True)
+    mpl.plot(fac_tot, ax=ax1, hemisphere='north', **plot_args)
+    ax1.annotate('(a)', xy=(0, 0), textcoords="axes fraction",
+                 xytext=(-0.1, 1.0), fontsize=18)
+
     ax2 = plt.subplot(122, projection='polar')
     mpl.plot(fac_tot, ax=ax2, hemisphere="south", style="contourf",
-             plot_opts="lin_-300_300", extend="both",
-             levels=50, drawcoastlines=True)
+             levels=50, extend="both", **plot_args)
+    ax2.annotate('(b)', xy=(0, 0), textcoords="axes fraction",
+                 xytext=(-0.1, 1.0), fontsize=18)
 
     if args.show:
         mpl.mplshow()
