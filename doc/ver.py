c40215dc257c29431f21c618803167acbd4391ae
Kristofor Maynard <kristofor.maynard@gmail.com>
2015-07-06 10:26:50 -0400

    bump to 0.90.1 dev

--- doc/ver.py
+++ doc/ver.py
@@ -1,7 +1,7 @@
 from distutils.version import LooseVersion
 
 # The short X.Y version.
-version = "0.90.0"
+version = "0.90.1 dev"
 
 # The full version, including alpha/beta/rc tags.
 release = LooseVersion(version)
