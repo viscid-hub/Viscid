695abf33c9a935c6d8e7dbcdcfd85c6fb274d4e7
Kristofor Maynard <kristofor.maynard@gmail.com>
2015-07-05 23:32:44 -0400

    better seed / streamline test

--- test/test_seed.py
+++ test/test_seed.py
@@ -2,46 +2,63 @@
 """ test using seeds to calc streamlines / interpolation """
 
 from __future__ import print_function
-from timeit import default_timer as time
 import argparse
 
-from viscid import logger
-from viscid import vutil
-from viscid.vlab import get_dipole
-from viscid.calculator import calc
-from viscid.calculator import cycalc
-from viscid.calculator import streamline
+import viscid
+from viscid.calculator import interp_trilin
 from viscid.plot import mpl
+from viscid.vlab import get_dipole
+
+def run_test(_fld, _seeds, plot_2d=True, plot_3d=True, selection=None,
+             show=False):
+    arr = interp_trilin(_fld, _seeds)
+    ifld = _seeds.wrap_field(arr)
+    pts = _seeds.points()
+    if plot_2d:
+        mpl.plot(ifld, selection=selection, show=show)
+    if plot_3d:
+        try:
+            from viscid.plot import mvi
+            mvi.mlab.points3d(pts[2], pts[1], pts[0], arr, scale_mode='none')
+            if show:
+                mvi.mlab.show()
+        except ImportError:
+            mpl.scatter_3d(pts, arr)
+            mpl.plt.gca().set_aspect('equal')
 
 def main():
-    parser = argparse.ArgumentParser(description="Load some data files")
-    parser.add_argument('files', nargs="*", help='input files')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--three", action="store_true")
     parser.add_argument("--show", "--plot", action="store_true")
-    args = vutil.common_argparse(parser)
+    args = viscid.vutil.common_argparse(parser)
     args = parser.parse_args()
 
-    logger.info("Testing field lines on 3d field...")
     B = get_dipole(l=[-10] * 3, h=[10] * 3, n=[128] * 3, m=[0.0, 0.0, -1.0])
+    bmag = viscid.calculator.calc.magnitude(B)
 
-    mygrid = B.crds.slice_keep("z=1.0f:3.0f,y=1:3,x=0.0f", cc=True)
+    ### PLANE
+    p0 = [0.5, 0.6, 0.7][::-1]
+    N = [1., 2., 3.][::-1]
+    L = [-3., -2., -1.][::-1]
+    plane = viscid.seed.Plane(p0, N, L, 2., 2., 15, 20)
+    run_test(bmag, plane, True, args.three, show=args.show)
 
-    logger.info("testing streamlines")
-    t0 = time()
-    lines, _ = streamline.streamlines(B, mygrid, ds0=0.01, ibound=0.05,
-                                      maxit=10000)
-    t1 = time()
-    logger.info("streamlines took {0:.3e}s to compute.".format(t1 - t0))
-    mpl.plot_streamlines(lines, show=args.show)
+    ### Sphere
+    p0 = [1.0, 1.0, 1.0][::-1]
+    sphere = viscid.seed.Sphere(p0, 1.0, 15, 20)
+    run_test(bmag, sphere, True, args.three, show=args.show)
 
-    logger.info("testing interp")
-    bmag = calc.magnitude(B)
-    t0 = time()
-    interp_vals = cycalc.interp_trilin(bmag, mygrid)
-    t1 = time()
-    logger.info("interp took {0:.3e}s to compute.".format(t1 - t0))
-    # interp_vals is now a 1d array of interpolated values
-    # interp_vals[i] is located at sphere.points[i]
-    mpl.scatter_3d(mygrid.points(center="Cell"), interp_vals, show=args.show)
+    ### Sphere Cap
+    p0 = [0.5, 0.6, 0.7][::-1]
+    p1 = [1.7, 1.6, 1.5][::-1]
+    sphere_cap = viscid.seed.SphericalCap(p0, p1, 120.0, 15, 20)
+    run_test(bmag, sphere_cap, True, args.three, show=args.show)
+
+    ### Circle
+    p0 = [0.5, 0.6, 0.7][::-1]
+    p1 = [0.7, 0.6, 0.5][::-1]
+    sphere_cap = viscid.seed.Circle(p0, p1, 20, r=0.8)
+    run_test(bmag, sphere_cap, True, args.three, show=args.show)
 
 if __name__ == "__main__":
     main()
--- test/test_streamline.py
+++ test/test_streamline.py
@@ -7,102 +7,46 @@ import argparse
 
 import numpy as np
 
-from viscid import logger
-from viscid import vutil
-from viscid import vlab
-from viscid.calculator import calc
-from viscid.calculator import cycalc
+import viscid
 from viscid.calculator import streamline
-from viscid.calculator import seed
 from viscid.plot import mpl
 
+
+def run_test(_fld, _seeds, show=False, try_mvi=True, **kwargs):
+    lines, topo = streamline.streamlines(_fld, _seeds, **kwargs)
+    topo_fld = _seeds.wrap_field(topo)
+    try:
+        if not try_mvi:
+            raise ImportError
+        from viscid.plot import mvi
+        mvi.plot_lines(lines)
+        if show:
+            mvi.mlab.show()
+    except ImportError:
+        mpl.plot_streamlines(lines, show=show)
+
 def main():
-    parser = argparse.ArgumentParser(description="Load some data files")
-    parser.add_argument('files', nargs="*", help='input files')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mayavi", action="store_true")
     parser.add_argument("--show", "--plot", action="store_true")
-    parser.add_argument("--old", action="store", type=int)
-    args = vutil.common_argparse(parser)
+    args = viscid.vutil.common_argparse(parser)
     args = parser.parse_args()
 
-    logger.info("Testing field lines on 2d field...")
-    B = vlab.get_dipole(twod=True)
+    viscid.logger.info("Testing field lines on 2d field...")
+    B = viscid.vlab.get_dipole(twod=True)
+    line = viscid.seed.Line((0.0, 0.0, 0.2), (0.0, 0.0, 1.0), 10)
     obound0 = np.array([-4, -4, -4], dtype=B.data.dtype)
     obound1 = np.array([4, 4, 4], dtype=B.data.dtype)
-    t0 = time()
-    lines, topo = streamline.streamlines(B,
-                                         seed.Line((0.0, 0.0, 0.2),
-                                                   (0.0, 0.0, 1.0),
-                                                   10),
-                                         ds0=0.01, ibound=0.1, maxit=10000,
-                                         obound0=obound0, obound1=obound1,
-                                         method=streamline.EULER1,
-                                         stream_dir=streamline.DIR_BOTH,
-                                         output=streamline.OUTPUT_BOTH,
-                                         tol_lo=5e-3, tol_hi=2e-1,
-                                         fac_refine=0.75, fac_coarsen=1.5)
-    t1 = time()
-    logger.info("streamlines took {0:.3e}s to compute.".format(t1 - t0))
-    mpl.plot_streamlines(lines, show=args.show)
+    run_test(B, line, show=args.show, try_mvi=args.mayavi,
+             obound0=obound0, obound1=obound1)
 
-    logger.info("Testing field lines on 3d field...")
-    B = vlab.get_dipole(m=[0.2, 0.3, -0.9])
-    t0 = time()
-    lines, topo = streamline.streamlines(B,
-                                         seed.Sphere((0.0, 0.0, 0.0),
-                                                     2.0, 20, 10),
-                                         ds0=0.01, ibound=0.1, maxit=10000,
-                                         method=streamline.RK12,
-                                         output=streamline.OUTPUT_STREAMLINES,
-                                         tol_lo=1e-3, tol_hi=1e-2,
-                                         fac_refine=0.75, fac_coarsen=2.0)
-    t1 = time()
-    logger.info("streamlines took {0:.3e}s to compute.".format(t1 - t0))
-    mpl.plot_streamlines(lines, show=args.show)
-
-    # assert(0)
-    logger.info("Testing trilinear interpolation...")
-
-    bmag = calc.magnitude(B)
-
-    plane = seed.Plane((1., 1., 1.), (1., 1., 1.), (0., 0., 1.), 2., 2., 50, 50)
-    t0 = time()
-    interp_vals = cycalc.interp_trilin(bmag, plane)
-    t1 = time()
-    logger.info("plane interp took {0:.3e}s to compute.".format(t1 - t0))
-    # interp_vals is now a 1d array of interpolated values
-    # interp_vals[i] is located at sphere.points[i]
-    mpl.scatter_3d(plane.points(), interp_vals, show=args.show)
-
-    vol = bmag.crds.slice("x=::32,y=::32,z=::32")
-    t0 = time()
-    interp_vals = cycalc.interp_trilin(bmag, vol)
-    t1 = time()
-    logger.info("volume interp took {0:.3e}s to compute.".format(t1 - t0))
-    # interp_vals is now a 1d array of interpolated values
-    # interp_vals[i] is located at sphere.points[i]
-    mpl.scatter_3d(vol.points(center=bmag.center), interp_vals, show=args.show)
-
-    sphere = seed.Sphere((0.0, 0.0, 0.0), 2.0, 200, 200)
-
-    # doing trilin interp on a scalar field
-    t0 = time()
-    interp_vals = cycalc.interp_trilin(bmag, sphere)
-    t1 = time()
-    logger.info("sphere interp took {0:.3e}s to compute.".format(t1 - t0))
-    # interp_vals is now a 1d array of interpolated values
-    # interp_vals[i] is located at sphere.points[i]
-    mpl.scatter_3d(sphere.points(), interp_vals, show=args.show)
-
-    # doing trilin interp on a vector field
-    t0 = time()
-    interp_vals = cycalc.interp_trilin(B, sphere)
-    t1 = time()
-    logger.info("vector interp took {0:.3e}s to compute.".format(t1 - t0))
-    # make a 3d scatter plot of bz
-    mpl.scatter_3d(sphere.points(), interp_vals[:, 2], show=args.show)
-
-    # val = cycalc.interp_trilin(bmag, seed.Point((1.0, 1.0, 1.0)))
-    # logger.info("bmag value at point (1, 1, 1) is {0}".format(val))
+    viscid.logger.info("Testing field lines on 3d field...")
+    B = viscid.vlab.get_dipole(m=[0.2, 0.3, -0.9])
+    sphere = viscid.seed.Sphere((0.0, 0.0, 0.0), 2.0, 20, 10)
+    obound0 = np.array([-4, -4, -4], dtype=B.data.dtype)
+    obound1 = np.array([4, 4, 4], dtype=B.data.dtype)
+    run_test(B, sphere, show=args.show, try_mvi=args.mayavi,
+             obound0=obound0, obound1=obound1, method=streamline.RK12)
 
 if __name__ == "__main__":
     main()
