dc0d5db4f145da64a0320cb5721e714b20a2e57b
Kristofor Maynard <kristofor.maynard@gmail.com>
2015-11-08 21:51:09 -0500

    bump to 0.95.0 dev

--- viscid/__init__.py
+++ viscid/__init__.py
@@ -13,7 +13,7 @@ Attributes:
         :py:func`viscid.vutil.common_argparse`.
 """
 
-__version__ = """0.94.0"""
+__version__ = """0.95.0 dev"""
 
 __all__ = ['amr_field',  # Modules
            'amr_grid',
