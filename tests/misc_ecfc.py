b9fcb621554251cb0660bcc0620a5d2f455a03e7
Kristofor Maynard <kristofor.maynard@gmail.com>
2017-02-21 16:10:03 -0500

    add interpolation / streamline to ecfc test (note this test is still not part of the suite)

--- tests/misc_ecfc.py
+++ tests/misc_ecfc.py
@@ -58,6 +58,8 @@ def main():
     test_fc = 1
     test_ec = 1
     test_div = 1
+    test_interp = 1
+    test_streamline = 1
 
     mhd_type = mhd_type.upper()
     if mhd_type.startswith("C"):
@@ -154,6 +156,98 @@ def main():
             #
             # if abs_max_rel_diff > rtol or np.any(np.abs(max_crd_diff) > catol):
             #     raise RuntimeError("Tolerance exceeded on divB calculation")
+
+    if test_streamline:
+        b_cc = f['b_cc']['x=-40f:12f, y=-15f:15f, z=-15f:15f']
+        b_fc = f['b_fc']['x=-40f:12f, y=-15f:15f, z=-15f:15f']
+
+        cotr = viscid.cotr.Cotr()
+        r_mask = 3.0
+        # set b_cc to dipole inside some sphere
+        isphere_mask = viscid.make_spherical_mask(b_cc, rmax=r_mask)
+        moment = cotr.get_dipole_moment(crd_system=b_cc)
+        viscid.fill_dipole(b_cc, m=moment, mask=isphere_mask)
+        # set b_fc to dipole inside some sphere
+        isphere_mask = viscid.make_spherical_mask(b_fc, rmax=r_mask)
+        moment = cotr.get_dipole_moment(crd_system=b_fc)
+        viscid.fill_dipole(b_fc, m=moment, mask=isphere_mask)
+
+        seeds = viscid.Volume([-10, 0, -5], [10, 0, 5], (16, 1, 3))
+        sl_kwargs = dict(ibound=1.0, method=viscid.EULER1A)
+        lines_cc, topo_cc = viscid.calc_streamlines(b_cc, seeds, **sl_kwargs)
+        lines_fc, topo_fc = viscid.calc_streamlines(b_fc, seeds, **sl_kwargs)
+
+        if make_plots:
+            mpl.plt.figure(figsize=(10, 6))
+
+            ax0 = mpl.plt.subplot(211)
+            topo_cc_colors = viscid.topology2color(topo_cc)
+            mpl.plot(f['pp']['y=0f'], logscale=True, earth=True, cmap='plasma')
+            mpl.plot2d_lines(lines_cc, topo_cc_colors, symdir='y')
+
+            ax0 = mpl.plt.subplot(212, sharex=ax0, sharey=ax0)
+            topo_fc_colors = viscid.topology2color(topo_fc)
+            mpl.plot(f['pp']['y=0f'], logscale=True, earth=True, cmap='plasma')
+            mpl.plot2d_lines(lines_fc, topo_fc_colors, symdir='y')
+
+            mpl.plt.xlim(-20, 10)
+            mpl.plt.ylim(-10, 10)
+            mpl.auto_adjust_subplots()
+            mpl.show()
+
+    if test_interp:
+        # test interpolation with E . B / B
+        b_cc = f['b_cc']
+        b_fc = f['b_fc']
+        e_cc = f['e_cc']
+        e_ec = f['e_ec']
+
+        cotr = viscid.cotr.Cotr()
+        r_mask = 3.0
+        # set b_cc to dipole inside some sphere
+        isphere_mask = viscid.make_spherical_mask(b_cc, rmax=r_mask)
+        moment = cotr.get_dipole_moment(crd_system=b_cc)
+        viscid.fill_dipole(b_cc, m=moment, mask=isphere_mask)
+        # set b_fc to dipole inside some sphere
+        isphere_mask = viscid.make_spherical_mask(b_fc, rmax=r_mask)
+        moment = cotr.get_dipole_moment(crd_system=b_fc)
+        viscid.fill_dipole(b_fc, m=moment, mask=isphere_mask)
+        # zero out e_cc inside some sphere
+        viscid.set_in_region(e_cc, e_cc, alpha=0.0, beta=0.0, out=e_cc,
+                             mask=viscid.make_spherical_mask(e_cc, rmax=r_mask))
+        # zero out e_ec inside some sphere
+        viscid.set_in_region(e_ec, e_ec, alpha=0.0, beta=0.0, out=e_ec,
+                             mask=viscid.make_spherical_mask(e_ec, rmax=r_mask))
+
+        tmp = viscid.empty([np.linspace(-10, 10, 64), np.linspace(-10, 10, 64),
+                            np.linspace(-10, 10, 64)], center="Cell")
+
+        b_cc_interp = viscid.interp_linear(b_cc, tmp)
+        b_fc_interp = viscid.interp_linear(b_fc, tmp)
+        e_cc_interp = viscid.interp_linear(e_cc, tmp)
+        e_ec_interp = viscid.interp_linear(e_ec, tmp)
+
+        epar_cc = viscid.dot(e_cc_interp, b_cc_interp) / viscid.magnitude(b_cc_interp)
+        epar_ecfc = viscid.dot(e_ec_interp, b_fc_interp) / viscid.magnitude(b_fc_interp)
+
+        if make_plots:
+            # mpl.plt.figure()
+            # ax0 = mpl.plt.subplot(121)
+            # mpl.plot(b_cc['x']['y=0f'], clim=(-40, 40))
+            # mpl.plt.subplot(122, sharex=ax0, sharey=ax0)
+            # mpl.plot(b_fc['x']['y=0f'], clim=(-40, 40))
+            # mpl.show()
+
+            mpl.plt.figure(figsize=(14, 5))
+            ax0 = mpl.plt.subplot(131)
+            mpl.plot(epar_cc['y=0f'], symmetric=True, cbarlabel="Epar CC")
+            mpl.plt.subplot(132, sharex=ax0, sharey=ax0)
+            mpl.plot(epar_ecfc['y=0f'], symmetric=True, cbarlabel="Epar ECFC")
+            mpl.plt.subplot(133, sharex=ax0, sharey=ax0)
+            mpl.plot(((epar_cc - epar_ecfc) / epar_cc)['y=0f'], clim=(-10, 10),
+                     cbarlabel="Rel Diff")
+            mpl.auto_adjust_subplots()
+            mpl.show()
 
     return 0
 
