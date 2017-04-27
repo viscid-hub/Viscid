45689fc66493120f5df0f84c78df30df390f25ed
Kristofor Maynard <kristofor.maynard@gmail.com>
2017-04-27 18:04:20 -0400

    add better fortran / cython benchmarking in scratch/

---
+++ scratch/fort_tools/__init__.py
@@ -0,0 +1,8 @@
+
+from fort_tools import tracer
+from fort_tools import fort_shims
+
+from fort_tools.fort_shims import fort_prep_field, fort_interp_trilin, fort_topology
+
+__all__ = ["tracer", "fort_shims",
+           "fort_prep_field", "fort_interp_trilin", "fort_topology"]
