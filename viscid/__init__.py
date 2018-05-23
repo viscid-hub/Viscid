296693dccd727734c88611df9339a37abe838841
Kristofor Maynard <kristofor.maynard@gmail.com>
2018-05-23 19:48:13 -0400

    bump version to 0.99.4 dev

--- viscid/__init__.py
+++ viscid/__init__.py
@@ -28,7 +28,7 @@ from viscid import _rc
 from viscid.compat.vimportlib import import_module
 
 
-__version__ = """0.99.3"""
+__version__ = """0.99.4 dev"""
 
 __all__ = ['amr_field',
            'amr_grid',
