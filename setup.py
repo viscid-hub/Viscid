4c78eebc3398caf8cf1649df62b9bfbdec90aa5f
Kristofor Maynard <kristofor.maynard@gmail.com>
2018-05-27 12:56:28 -0400

    another linux gfortran fix

--- setup.py
+++ setup.py
@@ -422,6 +422,11 @@ if sys.platform == 'darwin':
         our_ld_flags = " -undefined dynamic_lookup -bundle"
         os.environ['LDFLAGS'] = os.environ['LDFLAGS'] + our_ld_flags
 
+if sys.platform == 'linux':
+    if 'LDFLAGS' in os.environ:
+        our_ld_flags = " -shared"
+        os.environ['LDFLAGS'] = os.environ['LDFLAGS'] + our_ld_flags
+
 
 def get_viscid_version(init_py):
     with io.open(init_py, 'r', encoding="utf-8") as f:
