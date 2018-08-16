5363356341ec8421c4da54a874dcad41f066f91d
Kristofor Maynard <kristofor.maynard@gmail.com>
2018-08-16 16:07:04 -0400

    bump version to 0.99.9.dev3

--- CHANGES.md
+++ CHANGES.md
@@ -1,7 +1,7 @@
 Changes
 -------
 
-0.99.9.dev2
+0.99.9.dev3
 ===========
 
   - add `viscid.mfield` that mimics `numpy.mgrid`
--- viscid/__init__.py
+++ viscid/__init__.py
@@ -28,7 +28,7 @@ from viscid import _rc
 from viscid.compat.vimportlib import import_module
 
 
-__version__ = """0.99.9.dev2"""
+__version__ = """0.99.9.dev3"""
 
 __all__ = ['amr_field',
            'amr_grid',
