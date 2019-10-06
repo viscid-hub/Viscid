6daf0aeb73dcbad5773ae67e462a7f4a0555b554
Kristofor Maynard <kristofor.maynard@gmail.com>
2019-10-05 22:22:29 -0400

    bump version to 1.0.1.dev

--- viscid/__init__.py
+++ viscid/__init__.py
@@ -28,7 +28,7 @@ from viscid import _rc
 from viscid.compat.vimportlib import import_module
 
 
-__version__ = """1.0.0"""
+__version__ = """1.0.1.dev"""
 
 __all__ = ['amr_field',
            'amr_grid',
